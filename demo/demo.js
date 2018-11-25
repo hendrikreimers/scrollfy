6d1b431b773262bbbdae7503b3e796d90c8fe477
Hendrik <hendrik.reimers@gmail.com>
2018-11-25 16:20:34 +0100

    fixed on load event and modified demos

--- demo/demo.js
+++ demo/demo.js
@@ -1,24 +1,30 @@
 
 $(document).ready(function() {
+    // Scrollfy needs to be called at last, after the event listeners added, to be sure it will be visible on load
     $('div.block')
 		.addClass('down')
-		.scrollfy()
+
 		.on('scrollfy:scroll:begin', function(e) {
 			$(this).removeClass('up down').addClass(e.scrollfy.direction);
 		})
+
 		.on('scrollfy:inView', function(e) {
 			if ( !$(this).hasClass('inview') ) {
 				$(this).delay(100).addClass('inview');
 			}
 		})
+
 		.on('scrollfy:offView', function(e) {
 			if ( $(this).hasClass('inview') ) {
 				$(this).removeClass('inview');
 			}
 		})
+
 		.on('scrollfy:scroll:end', function(e) {
 			$(this).removeClass('up down');
-		});
+		})
+
+		.scrollfy(); // last step after listeners added
 	
 	$(window).on('scrollfy:scroll', function(e) {
 		if ( e.scrollfy.direction == 'up' ) {
--- demo/demo2.js
+++ demo/demo2.js
@@ -3,19 +3,22 @@ $(document).ready(function() {
 	$('div.block p').each(function(k,el) {
 		$(el).data('pos',$(el).position().top);
 	});
-	
+
+    // Scrollfy needs to be called at last, after the event listeners added, to be sure it will be visible on load
     $('div.block')
-		.scrollfy()
 		.on('scrollfy:inView', function(e) {
+			console.log('jap');
 			if ( !$(this).hasClass('inview') ) {
 				$(this).delay(100).addClass('inview');
 			}
 		})
+
 		.on('scrollfy:offView', function(e) {
 			if ( $(this).hasClass('inview') ) {
 				$(this).removeClass('inview');
 			}
 		})
+
 		.on('scrollfy:scroll', function(e) {
 			if ( $(this).hasClass('inview') ) {
 				var p = $(this).find('p');
@@ -34,5 +37,7 @@ $(document).ready(function() {
 			
 				$(p).css('top', + pos + 'px');
 			}
-		});
+		})
+
+		.scrollfy(); // last step after listeners added
 });
